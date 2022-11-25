b6bdaccbd5f48181212f4accb27f8ce5f0372559
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-11-25 18:36:20 +0100

    add value input default

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       <form action="./risposta.php" method="get">
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label" name="banna">Email address</label>
-            <input type="text" class="form-control" name="banna" id="exampleFormControlInput1" placeholder="parole da bannare">
+            <input type="text" class="form-control" name="banna" id="exampleFormControlInput1" :value="name">
           </div>
           <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
@@ -40,9 +40,10 @@
 createApp({
   data() {
     return {
+      name: 'walter white',
       message: `Walter White è un professore di chimica di Albuquerque, New Mexico, che vive con la moglie Skyler, incinta della loro secondogenita, e il figlio Walter "Flynn" Junior, affetto da una paralisi cerebrale che gli causa problemi di linguaggio e lo costringe a camminare con l'aiuto di stampelle per muoversi. Alla soglia dei cinquant'anni scopre di avere un tumore e per far fronte alle difficoltà economiche della famiglia, Walter è costretto a svolgere un secondo lavoro come dipendente di un autolavaggio. A tutto questo si aggiunge il profondo senso di insoddisfazione di Walter, che deve sopportare le angherie del suo titolare, dei suoi amici e familiari, i quali lo vedono come un uomo debole e remissivo. In particolare suo cognato Hank, agente della DEA, con cui peraltro ha un buon rapporto, non perde occasione per mettere a confronto la sua vita avventurosa con quella di Walter, totalmente priva di soddisfazioni.
 
-Quando, il giorno dopo il suo cinquantesimo compleanno, a Walter viene diagnosticato un cancro ai polmoni, i suoi problemi sembrano precipitare, anche perché non dispone della necessaria copertura assicurativa per curarsi. Tuttavia, in seguito al casuale incontro con Jesse Pinkman, un suo ex studente diventato uno spacciatore di poco conto, Walter decide di cominciare a cucinare insieme a lui cristalli di metanfetamina. Grazie alla sua conoscenza della chimica, il prodotto di Walter si rivela di qualità nettamente superiore rispetto alla concorrenza, con una purezza del 99,1%. Decide quindi di sfruttare le sue capacità per farsi, a mano a mano, sempre più strada nel mercato della droga e ricavare una quantità di denaro tale da lasciare al sicuro la sua famiglia nel momento in cui la malattia avrebbe preso il sopravvento.`
+      Quando, il giorno dopo il suo cinquantesimo compleanno, a Walter viene diagnosticato un cancro ai polmoni, i suoi problemi sembrano precipitare, anche perché non dispone della necessaria copertura assicurativa per curarsi. Tuttavia, in seguito al casuale incontro con Jesse Pinkman, un suo ex studente diventato uno spacciatore di poco conto, Walter decide di cominciare a cucinare insieme a lui cristalli di metanfetamina. Grazie alla sua conoscenza della chimica, il prodotto di Walter si rivela di qualità nettamente superiore rispetto alla concorrenza, con una purezza del 99,1%. Decide quindi di sfruttare le sue capacità per farsi, a mano a mano, sempre più strada nel mercato della droga e ricavare una quantità di denaro tale da lasciare al sicuro la sua famiglia nel momento in cui la malattia avrebbe preso il sopravvento.`
     }
   }
 }).mount('#app')
